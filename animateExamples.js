944be9a267d4a69c0335899ab9b9bdb7d2d400d5
Nathan <nathan.carter3252@gmail.com>
2020-08-28 17:21:12 +0100

    created text which shows usage of animation which is currently selected

--- animateExamples.js
+++ animateExamples.js
@@ -6,19 +6,25 @@ function hoverFunc() {
     logo.forEach((e) => e.classList.remove(previousClass))
   } 
   const hover = document.querySelector("#hover").value
-  previousClass = hover
-  logo.forEach((e) => e.classList.add(hover))
+  const data = hover.split("|");
+  previousClass = data[0]
+  logo.forEach((e) => e.classList.add(data[0]))
+  document.querySelector('#use').textContent = data[1]
 }
 
 
 function eventFunc() {
+  
   if(previousClass) {
-    logo.forEach((e) => e.classList.remove(previousClass))
+    logo[0].classList.remove(previousClass)
 
   } 
   const event = document.querySelector('#event').value
-  previousClass = event
-  logo[0].classList.add(event)
+  const data = event.split("|");
+  console.log(data)
+  previousClass = data[0]
+  logo[0].classList.add(data[0])
+  document.querySelector('#use').textContent = data[1]
 }
 
 
